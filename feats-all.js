448dbb63f83e54eb4385226e5ba9e10a808563ec
d180cf <d180cf@gmail.com>
2017-10-28 15:33:21 -0700

    Use all CPUs to compute features

--- feats-all.js
+++ feats-all.js
@@ -3,22 +3,31 @@
  * Output: JSON files with features.
  */
 
-const fs = require('fs');
-const fspath = require('path');
-const glob = require('glob');
-const feats = require('./feats');
+const [, script, inputFiles, outputDir, prefix] = process.argv;
 
-const [, , inputFiles, outputDir] = process.argv;
+if (!prefix) {
+    const pool = require('./proc-pool');
 
-console.log('Getting the list of files...');
-const paths = glob.sync(inputFiles);
+    for (let i = 0; i < 16; i++)
+        pool.run(`node ${script} ${inputFiles} ${outputDir} ${i.toString(16)}`);
+} else {
+    const fs = require('fs');
+    const fspath = require('path');
+    const glob = require('glob');
+    const feats = require('./feats');
+        
+    console.log('Getting the list of files...');
+    const paths = glob.sync(inputFiles);
 
-paths.forEach((path, index) => {
-    process.stdout.write(`${index / paths.length * 100 | 0} % = ${index} / ${paths.length}\r`);
-    
-    const name = fspath.basename(path, fspath.extname(path)) + '.json';
-    const output = fspath.join(outputDir, name);
+    console.log('Computing features...');
+    paths.forEach((path, index) => {
+        const name = fspath.basename(path, fspath.extname(path)) + '.json';
+        const output = fspath.join(outputDir, name);
 
-    if (!fs.existsSync(output))
-        feats.compute(path, output); // takes about 20 ms x 500 K files
-});
+        if (prefix && !name.startsWith(prefix))
+            return;
+
+        if (!fs.existsSync(output))
+            feats.compute(path, output); // takes about 20 ms x 500 K files
+    });
+}

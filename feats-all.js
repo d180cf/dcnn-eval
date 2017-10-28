7767f58920dd1899205dfb5a8be89f89eb087646
d180cf <d180cf@gmail.com>
2017-10-28 15:21:54 -0700

    Skip already computed boards

--- feats-all.js
+++ feats-all.js
@@ -3,6 +3,7 @@
  * Output: JSON files with features.
  */
 
+const fs = require('fs');
 const fspath = require('path');
 const glob = require('glob');
 const feats = require('./feats');
@@ -14,6 +15,10 @@ const paths = glob.sync(inputFiles);
 
 paths.forEach((path, index) => {
     process.stdout.write(`${index / paths.length * 100 | 0} % = ${index} / ${paths.length}\r`);
+    
     const name = fspath.basename(path, fspath.extname(path)) + '.json';
-    feats.compute(path, fspath.join(outputDir, name)); // takes about 20 ms x 500 K files
+    const output = fspath.join(outputDir, name);
+
+    if (!fs.existsSync(output))
+        feats.compute(path, output); // takes about 20 ms x 500 K files
 });
